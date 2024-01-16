620bf8a6151e7330349d210574852531750414a3
reginaldmj <reginald.jeffersonm@gmail.com>
2024-01-16 17:11:31 -0600

    App Component

---
+++ square/square.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-square',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<p>square works!</p>`,
+  styles: ``
+})
+export class SquareComponent {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-square',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class SquareComponent {
   title = 'TicTacToeApp';
 }
